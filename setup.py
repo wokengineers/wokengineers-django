14b141b494a2684aa11641d1e248bb92ccb56c5d
Shashank Singh <shashank.singh84335@gmail.com>
2023-09-23 20:42:02 +0530

    added changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(),
     description='This is a Django helper library by wokengineers',
     license='MIT',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/wokengineers/wokengineers-django.git',
     author='wokengineers',
     keywords=['pip','wokengineers','library',"django","loggers","middleware"]
